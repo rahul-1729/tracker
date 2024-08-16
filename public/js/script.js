86353c3ed3ae930a8afbc8eea1c15dd25ce2c5f1
rahul-1729 <rk9763981@gmail.com>
2024-08-16 13:34:11 +0530

    added set view

--- public/js/script.js
+++ public/js/script.js
@@ -2,8 +2,8 @@
  
  if(navigator.geolocation){
     navigator.geolocation.watchPosition((position)=>{
-        const {lattitude,longitude}=position.coords;
-        socket.emit("send-location",{lattitude,longitude});
+        const {latitude,longitude}=position.coords;
+        socket.emit("send-location",{latitude,longitude});
 
     },
     (error)=>{
@@ -15,20 +15,26 @@
         maximumAge:0,
     }
     );
- }
+ }  
 
  const map =L.map("map").setView([0,0],10);
 
  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",{
- attribution:"openStreetMap"
+ attribution:"OpenStreetMap"
  }).addTo(map)
 
  const markers={};
 
  socket.on("receive-location",(data)=>{
     const{id,latitude,longitude}=data;
-    map.setView([latitude,longitude],16);
+    map.setView([latitude,longitude],20);
 
-    
+    if(markers[id]){
+    markers[id].setLatLng([latitude,longitude]);
+    }
+    else
+    {
+      markers[id]=L.marker([latitude,longitude]).addTo(map);
+    }
     
 }) 
