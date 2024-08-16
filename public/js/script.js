ca97ccc51213efb3c5659c7158d0a6108cd571ef
rahul-1729 <rk9763981@gmail.com>
2024-08-16 12:41:12 +0530

    adding lattitude,longitude,altutude

--- public/js/script.js
+++ public/js/script.js
@@ -1,2 +1,24 @@
  const socket = io();
- console.log("hey")
+ 
+ if(navigator.geolocation){
+    navigator.geolocation.watchPosition((position)=>{
+        const {lattitude,longitude}=position.coords;
+        socket.emit("send-location",{lattitude,longitude});
+
+    },
+    (error)=>{
+        console.log(error);
+    },
+    {
+        enableHighAccuracy:true,
+        timeout:5000,
+        maximumAge:0,
+    }
+    );
+ }
+
+ L.map("map").setView([0,0],10);
+
+ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",{
+ attribution
+ })
