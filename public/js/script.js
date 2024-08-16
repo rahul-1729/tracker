62ce4a8ba06eb07bcac9c35d1005c70f9ae24dfc
rahul-1729 <rk9763981@gmail.com>
2024-08-16 13:06:54 +0530

    added map

--- public/js/script.js
+++ public/js/script.js
@@ -17,8 +17,8 @@
     );
  }
 
- L.map("map").setView([0,0],10);
+ const map =L.map("map").setView([0,0],10);
 
  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",{
  attribution:"openStreetMap"
- })
+ }).addTo(map)
